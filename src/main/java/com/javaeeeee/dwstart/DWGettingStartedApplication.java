dcbdfa0d480a73202f00cc54c1786dacb21d7483
javaeeeee <javaeeeee@gmail.com>
2015-11-06 02:19:22 -0800

    Added some code to enable database connection

--- src/main/java/com/javaeeeee/dwstart/DWGettingStartedApplication.java
+++ src/main/java/com/javaeeeee/dwstart/DWGettingStartedApplication.java
@@ -1,33 +1,51 @@
 package com.javaeeeee.dwstart;
 
 import com.javaeeeee.dwstart.auth.GreetingAuthenticator;
+import com.javaeeeee.dwstart.core.Employee;
 import com.javaeeeee.dwstart.core.User;
 import com.javaeeeee.dwstart.resources.HelloResource;
 import com.javaeeeee.dwstart.resources.SecuredHelloResource;
 import io.dropwizard.Application;
 import io.dropwizard.auth.AuthFactory;
 import io.dropwizard.auth.basic.BasicAuthFactory;
+import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.hibernate.HibernateBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class DWGettingStartedApplication
         extends Application<DWGettingStartedConfiguration> {
 
+    /**
+     * Hibernate bundle
+     */
+    private final HibernateBundle<DWGettingStartedConfiguration> hibernateBundle
+            = new HibernateBundle<DWGettingStartedConfiguration>(Employee.class) {
+                
+                @Override
+                public DataSourceFactory getDataSourceFactory(
+                        DWGettingStartedConfiguration configuration
+                ) {
+                    return configuration.getDataSourceFactory();
+                }
+                
+            };
+    
     public static void main(final String[] args) throws Exception {
         new DWGettingStartedApplication().run(args);
     }
-
+    
     @Override
     public String getName() {
         return "DWGettingStarted";
     }
-
+    
     @Override
     public void initialize(
             final Bootstrap<DWGettingStartedConfiguration> bootstrap) {
-        // TODO: application initialization
+        bootstrap.addBundle(hibernateBundle);
     }
-
+    
     @Override
     public void run(final DWGettingStartedConfiguration configuration,
             final Environment environment) {
@@ -40,5 +58,5 @@ public class DWGettingStartedApplication
         environment.jersey().register(new HelloResource());
         environment.jersey().register(new SecuredHelloResource());
     }
-
+    
 }
--- src/main/java/com/javaeeeee/dwstart/DWGettingStartedConfiguration.java
+++ src/main/java/com/javaeeeee/dwstart/DWGettingStartedConfiguration.java
@@ -2,7 +2,8 @@ package com.javaeeeee.dwstart;
 
 import io.dropwizard.Configuration;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.hibernate.validator.constraints.*;
+import io.dropwizard.db.DataSourceFactory;
+import javax.validation.Valid;
 import javax.validation.constraints.*;
 
 public class DWGettingStartedConfiguration extends Configuration {
@@ -17,6 +18,15 @@ public class DWGettingStartedConfiguration extends Configuration {
      */
     @NotNull
     private String password;
+    /**
+     * A factory used to connect to a relational database management system.
+     * Factories are used by Dropwizard to group together related configuration
+     * parameters such as database connection driver, URI, password etc.
+     */
+    @NotNull
+    @Valid
+    private DataSourceFactory dataSourceFactory
+            = new DataSourceFactory();
 
     /**
      * Login getter.
@@ -36,6 +46,17 @@ public class DWGettingStartedConfiguration extends Configuration {
     @JsonProperty
     public String getPassword() {
         return password;
+    }
+
+    /**
+     * A getter for the database factory.
+     *
+     * @return An instance of database factory deserialized from the
+     * configuration file passed as a command-line argument to the application.
+     */
+    @JsonProperty("database")
+    public DataSourceFactory getDataSourceFactory() {
+        return dataSourceFactory;
     }
 
 }
